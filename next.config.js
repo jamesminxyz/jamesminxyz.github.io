af59ad618a41e8e8c935bf47be9aaaa39665fade
Jamie Min <jamiemindesign@gmail.com>
2023-04-29 10:29:30 +1000

    update packagejson

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   output: 'export',
   // distDir: 'dist',
   // assetPrefix: './'
-  basePath: '/jamesminxyz',
+  basePath: '/jamesminxyz.github.io',
 }
 
 module.exports = nextConfig
